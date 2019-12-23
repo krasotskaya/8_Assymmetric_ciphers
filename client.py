a043f8258786a077fdc76f93dbb19f984d3975a5
krasotskaya <49969731+krasotskaya@users.noreply.github.com>
2019-12-23 13:03:39 +0300

    #2

--- client.py
+++ client.py
@@ -15,12 +15,31 @@ def calc_full_key(key_part_s, key_prim, key_publ_s):
     key_full = key_part_s ** key_prim % key_publ_s
     return key_full
 
+def coding (st, key):
+    s = list(st)
+    for i in range(len(s)):
+        j = ord(s[i])
+        j += key
+        j = chr(j)
+        s[i] = j
+
+    return(''.join(s))
+    
+def decoding (st, key):
+    s = list(st)
+    for i in range(len(s)):
+        j = ord(s[i])
+        j -= key
+        j = chr(j)
+        s[i] = j
+    return(''.join(s))
+
 import socket
 from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.204.42', 9090))
+sock.connect(('10.38.205.43', 9090))
 print('connection')
 key_prim = 199
 key_publ_m = 197
@@ -31,24 +50,30 @@ key_full_m = 0
 key_full_s = 0
 i = 0
 msg = ''
-while i != 3:
-    i += 1
-    if i == 1:
-        msg = str(key_publ_m)
-        sock.send(msg.encode())
-        key_publ_s = int(sock.recv(1024))
-        print(key_publ_s)
-    if i == 2:
-        key_part_m = calc_part_key(key_publ_m, key_prim, key_publ_s) 
-        msg = str(key_part_m)
-        sock.send(msg.encode())
-        key_part_s = int(sock.recv(1024))
-        print(key_part_s)
-    if i == 3:
-        key_full_m = calc_full_key(key_part_s, key_prim, key_publ_s)
-        print(key_part_s, key_prim, key_publ_s)
-        msg = str(key_full_m)
-        sock.send(msg.encode())
-        key_full_s = int(sock.recv(1024))
-        print(key_full_s)
+while True:
+    if i <= 3:
+        i += 1
+        if i == 1:
+            msg = str(key_publ_m)
+            sock.send(msg.encode())
+            key_publ_s = int(sock.recv(1024))
+        if i == 2:
+            key_part_m = calc_part_key(key_publ_m, key_prim, key_publ_s) 
+            msg = str(key_part_m)
+            sock.send(msg.encode())
+            key_part_s = int(sock.recv(1024))
+        if i == 3:
+            key_full_m = calc_full_key(key_part_s, key_prim, key_publ_s)
+            print(key_part_s, key_prim, key_publ_s)
+            msg = str(key_full_m)
+            sock.send(msg.encode())
+            key_full_s = int(sock.recv(1024))
+            print(key_full_s)
+    else:
+        msg = input()
+        msg_new = coding(msg,key_full_m)
+        sock.send(msg_new.encode())
+        msg = sock.recv(1024).decode()
+        msg_new = decoding(msg,key_full_m)
+        print('m from server:\t', msg_new)
 sock.close()
--- server.py
+++ server.py
@@ -15,6 +15,25 @@ def calc_full_key(key_part_s, key_prim, key_publ_m):
     key_full = key_part_s ** key_prim % key_publ_m
     return key_full
 
+def coding (st, key):
+    s = list(st)
+    for i in range(len(s)):
+        j = ord(s[i])
+        j += key
+        j = chr(j)
+        s[i] = j
+
+    return(''.join(s))
+    
+def decoding (st, key):
+    s = list(st)
+    for i in range(len(s)):
+        j = ord(s[i])
+        j -= key
+        j = chr(j)
+        s[i] = j
+    return(''.join(s))
+
 import socket
 
 sock = socket.socket()
@@ -32,24 +51,29 @@ key_part_s = 0
 key_full_m = 0
 key_full_s = 0
 msg = ''
-while i != 3:
-    i += 1
-    if i == 1:
-        key_publ_s = int(conn.recv(1024))
-        msg = str(key_publ_m)
-        conn.send(msg.encode())
-        print(key_publ_s)
-    if i == 2:
-        key_part_s = int(conn.recv(1024))
-        key_part_m = calc_part_key(key_publ_s, key_prim, key_publ_m)
-        msg = str(key_part_m)
-        conn.send(msg.encode())
-        print(key_part_s)
-    if i == 3:
-        key_full_s = int(conn.recv(1024))
-        key_full_m = calc_full_key(key_part_s, key_prim, key_publ_m)
-        msg = str(key_full_m)
-        conn.send(msg.encode())
-        print(key_full_s)
-    	
+while True:
+    if i <= 3:
+        i += 1
+        if i == 1:
+            key_publ_s = int(conn.recv(1024))
+            msg = str(key_publ_m)
+            conn.send(msg.encode())
+        if i == 2:
+            key_part_s = int(conn.recv(1024))
+            key_part_m = calc_part_key(key_publ_s, key_prim, key_publ_m)
+            msg = str(key_part_m)
+            conn.send(msg.encode())
+        if i == 3:
+            key_full_s = int(conn.recv(1024))
+            key_full_m = calc_full_key(key_part_s, key_prim, key_publ_m)
+            msg = str(key_full_m)
+            conn.send(msg.encode())
+            print(key_full_s)
+    else:
+        msg = conn.recv(1024).decode()
+        msg_new = decoding(msg,key_full_m)
+        print('m from server:\t', msg_new)
+        msg = input()
+        msg_new = coding(msg,key_full_m)
+        conn.send(msg_new.encode())
 conn.close()
