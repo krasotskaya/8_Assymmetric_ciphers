e37bc89e680458841adaa081e705d9bdee387cb6
krasotskaya <49969731+krasotskaya@users.noreply.github.com>
2019-12-23 12:22:13 +0300

    #1

--- client.py
+++ client.py
@@ -1,14 +1,54 @@
-import socket
-import pickle
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 16:00:08 2019
 
-HOST = '127.0.0.1'
-PORT = 8080
+@author: Елизавета
+"""
+
+#client
+
+def calc_part_key(key_publ_m, key_prim, key_publ_s):
+    key_part_m = key_publ_m ** key_prim % key_publ_s
+    return key_part_m
+
+def calc_full_key(key_part_s, key_prim, key_publ_s):
+    key_full = key_part_s ** key_prim % key_publ_s
+    return key_full
+
+import socket
+from time import sleep
 
 sock = socket.socket()
-sock.connect((HOST, PORT))
-
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
-
+sock.setblocking(1)
+sock.connect(('10.38.204.42', 9090))
+print('connection')
+key_prim = 199
+key_publ_m = 197
+key_publ_s = 0
+key_part_m = 0
+key_part_s = 0
+key_full_m = 0
+key_full_s = 0
+i = 0
+msg = ''
+while i != 3:
+    i += 1
+    if i == 1:
+        msg = str(key_publ_m)
+        sock.send(msg.encode())
+        key_publ_s = int(sock.recv(1024))
+        print(key_publ_s)
+    if i == 2:
+        key_part_m = calc_part_key(key_publ_m, key_prim, key_publ_s) 
+        msg = str(key_part_m)
+        sock.send(msg.encode())
+        key_part_s = int(sock.recv(1024))
+        print(key_part_s)
+    if i == 3:
+        key_full_m = calc_full_key(key_part_s, key_prim, key_publ_s)
+        print(key_part_s, key_prim, key_publ_s)
+        msg = str(key_full_m)
+        sock.send(msg.encode())
+        key_full_s = int(sock.recv(1024))
+        print(key_full_s)
 sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,55 @@
-import socket
-import pickle
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 15:55:48 2019
 
-HOST = '127.0.0.1'
-PORT = 8080
+@author: Елизавета
+"""
+
+#server
+
+def calc_part_key(key_publ_s, key_prim, key_publ_m):
+    key_part_m = key_publ_s ** key_prim % key_publ_m
+    return key_part_m
+
+def calc_full_key(key_part_s, key_prim, key_publ_m):
+    key_full = key_part_s ** key_prim % key_publ_m
+    return key_full
+
+import socket
 
 sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
+sock.bind(('', 9090))
+print('connection')
+sock.listen(3)
+i = 0
 conn, addr = sock.accept()
-
-msg = conn.recv(1024)
-print(pickle.loads(msg))
-
+print(addr)
+key_publ_m = 151
+key_prim = 157
+key_publ_s = 0
+key_part_m = 0
+key_part_s = 0
+key_full_m = 0
+key_full_s = 0
+msg = ''
+while i != 3:
+    i += 1
+    if i == 1:
+        key_publ_s = int(conn.recv(1024))
+        msg = str(key_publ_m)
+        conn.send(msg.encode())
+        print(key_publ_s)
+    if i == 2:
+        key_part_s = int(conn.recv(1024))
+        key_part_m = calc_part_key(key_publ_s, key_prim, key_publ_m)
+        msg = str(key_part_m)
+        conn.send(msg.encode())
+        print(key_part_s)
+    if i == 3:
+        key_full_s = int(conn.recv(1024))
+        key_full_m = calc_full_key(key_part_s, key_prim, key_publ_m)
+        msg = str(key_full_m)
+        conn.send(msg.encode())
+        print(key_full_s)
+    	
 conn.close()
