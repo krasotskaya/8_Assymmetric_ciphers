a10e8d440b0c2812b6863413383601fe23529ef3
krasotskaya <49969731+krasotskaya@users.noreply.github.com>
2019-12-23 17:12:23 +0300

    1, 2, 3 дополнительные

--- client.py
+++ client.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
+
 """
+
 Created on Tue Dec 17 16:00:08 2019
 
+
+
 @author: Елизавета
+
 """
 
+
+
 #client
+
+
 
 def calc_part_key(key_publ_m, key_prim, key_publ_s):
     key_part_m = key_publ_m ** key_prim % key_publ_s
     return key_part_m
 
+
 def calc_full_key(key_part_s, key_prim, key_publ_s):
     key_full = key_part_s ** key_prim % key_publ_s
     return key_full
+
+
 
 def coding (st, key):
     s = list(st)
@@ -22,9 +34,9 @@ def coding (st, key):
         j += key
         j = chr(j)
         s[i] = j
-
     return(''.join(s))
-    
+
+
 def decoding (st, key):
     s = list(st)
     for i in range(len(s)):
@@ -34,46 +46,53 @@ def decoding (st, key):
         s[i] = j
     return(''.join(s))
 
-import socket
-from time import sleep
-
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.205.43', 9090))
-print('connection')
-key_prim = 199
-key_publ_m = 197
-key_publ_s = 0
-key_part_m = 0
-key_part_s = 0
-key_full_m = 0
-key_full_s = 0
-i = 0
-msg = ''
-while True:
-    if i <= 3:
-        i += 1
-        if i == 1:
+def gener(i, key_prim, key_publ_m):
+    if i == 1:
             msg = str(key_publ_m)
             sock.send(msg.encode())
             key_publ_s = int(sock.recv(1024))
-        if i == 2:
+    if i == 2:
             key_part_m = calc_part_key(key_publ_m, key_prim, key_publ_s) 
             msg = str(key_part_m)
             sock.send(msg.encode())
             key_part_s = int(sock.recv(1024))
-        if i == 3:
+    if i == 3:
             key_full_m = calc_full_key(key_part_s, key_prim, key_publ_s)
             print(key_part_s, key_prim, key_publ_s)
             msg = str(key_full_m)
             sock.send(msg.encode())
             key_full_s = int(sock.recv(1024))
             print(key_full_s)
-    else:
-        msg = input()
-        msg_new = coding(msg,key_full_m)
-        sock.send(msg_new.encode())
-        msg = sock.recv(1024).decode()
-        msg_new = decoding(msg,key_full_m)
-        print('m from server:\t', msg_new)
+            with open ('keys_c.txt','w') as f:
+                f.write(str(key_full_s))
+                
+def mess(sock, key_full_m):
+    msg = input('m from you:\t')
+    msg_new = coding(msg,key_full_m)
+    sock.send(msg_new.encode())
+    msg = sock.recv(1024).decode()
+    msg_new = decoding(msg,key_full_m)
+    print('m from server:\t', msg_new)
+        
+import socket
+sock = socket.socket()
+sock.setblocking(1)
+sock.connect(('localhost', 9090))
+print('connection')
+try:
+    with open ('keys_c.txt','r') as f:
+        for line in f:
+            key_full_m = int(line);
+except:
+    key_prim = 199
+    key_publ_m = 197
+    i = 0
+    msg = ''
+
+    while i<3:
+        i += 1
+        gener(i, key_prim, key_publ_m)
+while True:    
+    mess(sock, key_full_m)
+
 sock.close()
--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
+
 """
+
 Created on Tue Dec 17 15:55:48 2019
 
+
+
 @author: Елизавета
+
 """
 
+
+
 #server
+
+
 
 def calc_part_key(key_publ_s, key_prim, key_publ_m):
     key_part_m = key_publ_s ** key_prim % key_publ_m
@@ -24,7 +33,7 @@ def coding (st, key):
         s[i] = j
 
     return(''.join(s))
-    
+
 def decoding (st, key):
     s = list(st)
     for i in range(len(s)):
@@ -34,8 +43,42 @@ def decoding (st, key):
         s[i] = j
     return(''.join(s))
 
-import socket
+def gener(i, key_prim, key_publ_m):
+    if i == 1:
+            key_publ_s = int(conn.recv(1024))
+            msg = str(key_publ_m)
+            conn.send(msg.encode())
+    if i == 2:
+            key_part_s = int(conn.recv(1024))
+            key_part_m = calc_part_key(key_publ_s, key_prim, key_publ_m)
+            msg = str(key_part_m)
+            conn.send(msg.encode())
+    if i == 3:
+            key_full_s = int(conn.recv(1024))
+            key_full_m = calc_full_key(key_part_s, key_prim, key_publ_m)
+            msg = str(key_full_m)
+            conn.send(msg.encode())
+            print(key_full_s)
+            with open ('keys_s.txt','w') as f:
+                f.write(str(key_full_m))
+    
+def mess(conn, key_full_m):
+    msg = conn.recv(1024).decode()
+    msg_new = decoding(msg,key_full_m)
+    print('m from server:\t', msg_new)
+    msg = input('m from you:\t')
+    msg_new = coding(msg,key_full_m)
+    conn.send(msg_new.encode())
+    
+def check(key_publ_s):
+    i = False
+    with open ('key_list.txt', 'r') as f:
+        for line in f:
+            if line == str(key_publ_s):
+                i = True
+    return i
 
+import socket
 sock = socket.socket()
 sock.bind(('', 9090))
 print('connection')
@@ -43,37 +86,19 @@ sock.listen(3)
 i = 0
 conn, addr = sock.accept()
 print(addr)
-key_publ_m = 151
-key_prim = 157
-key_publ_s = 0
-key_part_m = 0
-key_part_s = 0
-key_full_m = 0
-key_full_s = 0
-msg = ''
-while True:
-    if i <= 3:
+try:
+    with open ('keys_s.txt','r') as f:
+        for line in f:
+            key_full_m = int(line);
+except:
+    key_publ_m = 151
+    key_prim = 157
+    i = 0
+    msg = ''
+    while i<3:
         i += 1
-        if i == 1:
-            key_publ_s = int(conn.recv(1024))
-            msg = str(key_publ_m)
-            conn.send(msg.encode())
-        if i == 2:
-            key_part_s = int(conn.recv(1024))
-            key_part_m = calc_part_key(key_publ_s, key_prim, key_publ_m)
-            msg = str(key_part_m)
-            conn.send(msg.encode())
-        if i == 3:
-            key_full_s = int(conn.recv(1024))
-            key_full_m = calc_full_key(key_part_s, key_prim, key_publ_m)
-            msg = str(key_full_m)
-            conn.send(msg.encode())
-            print(key_full_s)
-    else:
-        msg = conn.recv(1024).decode()
-        msg_new = decoding(msg,key_full_m)
-        print('m from server:\t', msg_new)
-        msg = input()
-        msg_new = coding(msg,key_full_m)
-        conn.send(msg_new.encode())
+        gener(i, key_prim, key_publ_m)
+while True:
+    mess(conn, key_full_m)
+
 conn.close()
